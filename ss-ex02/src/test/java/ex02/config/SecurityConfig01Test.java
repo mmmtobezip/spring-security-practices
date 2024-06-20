035365dc2479509fb45395f0fa1d801ede95b07b
mmmtobezip <mmmszip@gmail.com>
2024-06-20 14:16:46 +0900

    feat: spring scurity ex01 & ex02

---
+++ ss-ex02/src/test/java/ex02/config/SecurityConfig01Test.java
@@ -0,0 +1,13 @@
+package ex02.config;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {WebConfig.class, SecurityConfig01.class})
+@WebAppConfiguration
+public class SecurityConfig01Test {
+
+}
